06464460742a7f8e8da725da263578885b0614ae
Albert-Rangel <albertsleyther@gmail.com>
2023-11-11 11:05:41 -0400

    c

---
+++ src/dao/controllers/CartManager.js
@@ -0,0 +1,127 @@
+import { cartsModel } from '../models/carts.model.js';
+import { productsModel } from '../models/products.model.js';
+import ProductManager from './ProductManager.js';
+import cartsService from '../../services/cartService.js'
+const CartsService = new cartsService()
+const productManager = new ProductManager()
+
+class CartManager {
+
+  async addCart() {
+
+    try {
+      let carnew
+      return carnew = await CartsService.addCartviaService()
+
+    } catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async addCartProducts(pid, cid) {
+
+    try {
+      const answer = await CartsService.addCartProductsviaService(pid, cid)
+
+      if (answer == undefined || answer.length === 0) return `E02|El carro con el id ${cid} no se encuentra agregado.`;
+
+      return answer
+
+    } catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async getCarts() {
+    try {
+      const allCarts = await CartsService.getcartsviaService();
+      return allCarts
+
+    } catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async getCartById(cid) {
+    try {
+
+      const CartById = await CartsService.getCartbyIDviaService(cid)
+
+      if (CartById == undefined) return `E02|El carro con el id ${cid} no se encuentra agregado.`;
+
+      return CartById
+
+    } catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async getProductsinCartById(cid) {
+    try {
+      const cartObject = await CartsService.getProductsinCartbyIDviaService(cid)
+
+      return cartObject
+
+    } catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async deleteCart(cid) {
+    try {
+      return await CartsService.deleteCartviaService({ _id: cid })
+    }
+    catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async deleteCartProduct(pid, cid) {
+    try {
+      return await CartsService.deleteCartProductviaService(pid, cid)
+    }
+    catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async deleteAllCartProducts(cid) {
+    try {
+      const CartById = await CartsService.deleteAllCartProductsviaService(cid)
+
+      return CartById
+    }
+    catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async updateCartProductQuantity(pid, cid, quantity_) {
+    try {
+
+
+      const CartById = await CartsService.updateProductQuantityviaService(pid, cid, quantity_)
+
+      return CartById
+    }
+    catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+
+  async updateCartProducts(cid, products) {
+    try {
+      const answer = await CartsService.updateCartProducstviaService(cid, products)
+      
+      return answer
+    }
+    catch (error) {
+      return `ERR|Error generico. Descripcion :${error}`
+    }
+  }
+}
+
+export default CartManager;
+
+
+
---
+++ src/router/session.router.js
@@ -0,0 +1,93 @@
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+import { userModel } from '../dao/models/user.model.js';
+import publicRoutes from "../dao/middlewares/publicRoutes.js"
+import passport from 'passport';
+import privateRoutes from '../dao/middlewares/privateRoutes.js';
+import {fork} from "child_process"
+
+const router = Router();
+
+router.post(
+  '/login', publicRoutes,
+  passport.authenticate('login', { failureRedirect: '/failogin' }),
+  async (req, res) => {
+    if (!req.user) {
+      res.status(400).send();
+    }
+
+    req.session.user = {
+      firstname: req.user.firstname,
+      lastname: req.user.lastname,
+      email: req.user.email,
+      age: req.user.age,
+      admin: false,
+      role: req.user.role,
+    };
+    req.session.isLogged = true;
+
+    res.redirect('/products');
+  }
+);
+
+router.post('/signup', publicRoutes, passport.authenticate("register",
+  { failureRedirect: "/failsignup" }),
+  async (req, res) => {
+    res.redirect('/login');
+  });
+
+router.post('/recover', publicRoutes, async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).lean();
+
+  if (!user) {
+    return res.send(
+      'Si tu correo existe en nuestros registros, recibiras un mail con la información para recuperar tu contraseña'
+    );
+  }
+
+  user.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  await userModel.updateOne({ email }, user);
+
+  res.redirect('/login');
+});
+
+router.get("/github",
+  passport.authenticate("github", { scope: ["user:email"] }))
+
+router.get("/githubcallback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  (req, res) => {
+    req.session.user = {
+      firstname: req.user.firstname,
+      lastname: req.user.lastname,
+      email: req.user.email,
+      age: req.user.age,
+      role: req.user.role,
+      admin: false
+    };
+    req.session.isLogged = true;
+
+    res.redirect("/products")
+  })
+
+router.get(
+  '/current', privateRoutes,
+  async (req, res) => {
+    res.send(req.user);
+  }
+);
+
+router.get("/suma", (req, res) => {
+  const child= fork("./operacioncompleja.js")
+
+  child.send("hola")
+  child.on("message", (result)=>{
+
+    res.send(`el resultado de la operacion es ; ${result}`)
+  })
+})
+
+
+
+export default router;
